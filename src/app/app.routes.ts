aa87f4588251db65a479e87dfb8d74b99d8de322
DevinBowen <bowen61496@gmail.com>
2025-05-10 15:56:28 -0400

    v1.0 Added new auth components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,29 @@ import { AuthComponent } from './components/auth/auth.component'
 import { HomeComponent } from './components/home/home.component'
 
 export const routes: Routes = [
-        { path: '', component: HomeComponent },
+        { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
         {
             path: 'auth',
             component: AuthComponent,
             children: [
-                { path: 'login', component: AuthComponent }
+                { path: '', redirectTo: 'login', pathMatch: 'full' },
+                {
+                    path: 'login',
+                    loadComponent: () =>
+                    import('./components/auth/login/login.component').then(m => m.LoginComponent)
+                },
+                {
+                    path: 'register',
+                    loadComponent: () =>
+                    import('./components/auth/register/register.component').then(m => m.RegisterComponent)
+                },
+                {
+                    path: 'logout',
+                    loadComponent: () =>
+                    import('./components/auth/logout/logout.component').then(m => m.LogoutComponent)
+                }
             ]
         },
         { path: 'home', component: HomeComponent },
         { path: '**', redirectTo: '' }
-    ];
+    ]
